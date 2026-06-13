40e232e24c6c859c52a39fd0ea36d5f9926ff501
khaled alrefaee <116975678+khaledalrefaee@users.noreply.github.com>
2026-06-12 18:17:03 -0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,7 @@ Route::get('/branch/{branch}', [FrontController::class, 'branchShow'])->name('fr
 Route::get('/about', [FrontController::class, 'about'])->name('front.about');
 Route::get('/contact', [FrontController::class, 'contact'])->name('front.contact');
 Route::post('/contact', [FrontController::class, 'contactSend'])->name('front.contact.send');
+
 Route::get('/test-gd', function () {
     return extension_loaded('gd') ? 'GD ON' : 'GD OFF';
 });
