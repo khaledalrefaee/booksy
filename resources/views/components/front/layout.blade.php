6f3527b92ac9130f13814b3dd19e6800bf1d98c2
khaled alrefaee <khaledalrefae69@gmail.com>
2026-09-04 17:50:50 -0700

    maps

--- resources/views/components/front/layout.blade.php
+++ resources/views/components/front/layout.blade.php
@@ -99,6 +99,7 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
 <meta name="csrf-token" content="{{ csrf_token() }}">
+<meta name="referrer" content="strict-origin-when-cross-origin">
 
 {{-- ══════════════  PRIMARY SEO  ══════════════ --}}
 <title>{{ $title }}</title>
--- resources/views/owner/partials/css.blade.php
+++ resources/views/owner/partials/css.blade.php
@@ -1,5 +1,6 @@
 <meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="referrer" content="strict-origin-when-cross-origin">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
   <meta name="description" content="Responsive HTML Admin Dashboard Template based on Bootstrap 5">
 	<meta name="author" content="NobleUI">
